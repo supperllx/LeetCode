70e507f8f252badc5b255ff36bcde1d584ff8297
supperllx <supperllx@outlook.com>
2020-09-10 19:43:43 -0400

    add 1137 on t480s

--- test/test.py
+++ test/test.py
@@ -1,4 +1,24 @@
-a,b = 1,2
-print("{a}A{b}B".format(a = a, b= b))
-s1,s2 = '1123', '0111'
-print(s1 ^ s2)
+import collections
+
+def func(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    else:
+        return func(n-1) + func(n-2)
+
+def fab(n: int) -> int: 
+    if (n == 1):
+        return 1
+    elif (n == 2):
+        return 1
+    else:
+        return fab(n-1) + fab(n-2)
+
+if __name__ == "__main__":
+    dq = collections.deque()
+    dq.append(5)
+    dq.appendleft(2)
+    print(dq)
+    print(len(dq))
